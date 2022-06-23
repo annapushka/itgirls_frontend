44c4d96f5172be5ae2620aee79c61ff11436a1e0
Anna Pushkareva <smirnova_spb@mail.ru>
2022-06-23 21:41:07 +0300

    functions added

---
+++ JS_03_gallery/index.js
@@ -0,0 +1,9 @@
+function prev() {
+let image = document.getElementById('gallery__img');
+image.src ='assets/img/1.jpg';
+}
+
+function next() {
+    let image = document.getElementById('gallery__img');
+    image.src ='assets/img/3.jpg';
+}
