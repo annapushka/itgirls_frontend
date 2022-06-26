0058f7767b2d8d0b714021a4eba598354926fdd4
Anna Pushkareva <smirnova_spb@mail.ru>
2022-06-26 12:06:37 +0300

    date format new

---
+++ JS_10_date/index.js
@@ -0,0 +1,34 @@
+// Если спустя date прошло менее 1 секунды, вывести "прямо сейчас".
+// В противном случае, если с date прошло меньше 1 минуты, вывести "n сек. назад".
+// В противном случае, если меньше часа, вывести "m мин. назад".
+// В противном случае, полная дата в формате "DD.MM.YY HH:mm". А именно: "день.месяц.год часы:минуты",
+//  всё в виде двух цифр, т.е. 31.12.16 10:00.
+
+
+let askDate = prompt("Введите дату", '2022-06-26T10:00:00');
+
+const date = Date.parse(askDate);
+formatDate(date);
+
+function formatDate(date) {
+  const now = new Date();
+  let result = now - date;
+  console.log(result);
+
+  if (result < 1000) {
+    alert("прямо сейчас");
+  } else if (result < 1000 * 60) {
+    alert(`${Math.round(result / 1000)} сек. назад`);
+  } else if (result < 1000 * 60 * 60) {
+    alert(`${Math.round(result / 1000 / 60)} мин. назад`);
+  } else {
+    const printDate = new Date(date);
+    const day = printDate.getDate();
+    const month = printDate.getMonth();
+    const year = (printDate.getFullYear()).toString().slice(-2);
+    const hour = printDate.getHours();
+    const min = printDate.getMinutes();
+    const print = (day < 10 ? '0' : '') + day + ":" + (month < 10 ? '0' : '') + month + ":" + year + " " + (hour < 10 ? '0' : '') + hour + ":" + (min < 10 ? '0' : '') + min;
+    alert(print);
+  }
+}
