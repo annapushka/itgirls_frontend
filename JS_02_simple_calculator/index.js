4207a829dd3bcc76c800d543b4e0c954b93e3534
Anna Pushkareva <smirnova_spb@mail.ru>
2022-07-08 10:15:21 +0300

    class Calculator and static methods were added

--- JS_02_simple_calculator/index.js
+++ JS_02_simple_calculator/index.js
@@ -1,41 +1,58 @@
-function sum() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let result;
-    result = num1 + num2;
-    document.getElementById("result").innerHTML = result;
-}
-
-function subtract() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let result;
-    result = num1 - num2;
-    document.getElementById("result").innerHTML = result;
-}
-
-function multiply() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let result;
-    result = num1 * num2;
-    document.getElementById("result").innerHTML = result;
-}
-
-function divide() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let result;
-
-    if (num2 === 0) {
-        result = "(－‸ლ)";
-        document.getElementById("result").innerHTML = result;
-        return;
+let result = 0;
+class Calculator {
+    static sum(num1, num2) {
+        return result = num1 + num2;
     }
-    result = num1 / num2;
-    document.getElementById("result").innerHTML = result;
+
+    static subtract(num1, num2) {
+        return result = num1 - num2;
+    }
+
+    static multiply(num1, num2) {
+        return result = num1 * num2;
+    }
+
+    static divide(num1, num2) {
+        if (num2 === 0) {
+            return result = "(－‸ლ)";
+        }
+        return result = num1 / num2;
+    }
+}
+
+
+function calculateSum() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    Calculator.sum(num1, num2);
+    showResult(result);
+}
+
+function calculateSubstract() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    Calculator.subtract(num1, num2);
+    showResult(result);
+}
+
+function calculateMultiply() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    Calculator.multiply(num1, num2);
+    showResult(result);
+}
+
+function calculateDivade() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    Calculator.divide(num1, num2);
+    showResult(result);
 }
 
 function select(sender) {
     sender.classList.add('selected');
+}
+
+function showResult(result) {
+    document.getElementById("result").innerHTML = result;
 }
