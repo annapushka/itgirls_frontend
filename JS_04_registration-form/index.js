3e7d3dd9ed7a7cdafe3960588d9b32c56b43a26c
Anna Pushkareva <smirnova_spb@mail.ru>
2022-07-18 10:46:58 +0300

    post method was added

--- JS_04_registration-form/index.js
+++ JS_04_registration-form/index.js
@@ -1,14 +1,39 @@
-function check() {
-    const name = document.getElementById('exampleInputName');
-    const surname = document.getElementById("exampleInputSurname");
-    const year = document.getElementById("exampleInputBirthYear");
-    const email = document.getElementById("exampleInputEmail1");
-    const password = document.getElementById("exampleInputPassword1");
-    const password2 = document.getElementById("exampleInputPassword2");
+const name = document.getElementById('exampleInputName');
+const surname = document.getElementById("exampleInputSurname");
+const year = document.getElementById("exampleInputBirthYear");
+const email = document.getElementById("exampleInputEmail1");
+const password = document.getElementById("exampleInputPassword1");
+const password2 = document.getElementById("exampleInputPassword2");
+const postButton = document.getElementById("postButton");
+
+
+postButton.onclick = function (e) {
+    e.preventDefault();
+    let user = {
+        name: name.value,
+        surname: surname.value,
+        year: year.value,
+        email: email.value,
+        password: password.value
+    }
+    if(isChecked() === true) {
+        fetch("https://httpbin.org/post", 
+        {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+        })
+    }
+}
+
+function isChecked() {
     const password_len = password.value.length;
     const constters = /^[А-Яа-я]+$/;
     const mailformat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const year_len = year.value.length;
+
     let name_result = false;
     let surname_result = false;
     let year_result = false;
@@ -21,27 +46,42 @@ function check() {
 
     if (name.value == '' || !name.value.match(constters)) {
         document.getElementById('message').innerHTML += "Введите корректное имя<br>";
-    } else {name_result = true;}
+    } else {
+        name_result = true;
+    }
     if (surname.value == '' || !name.value.match(constters)) {
         document.getElementById("message").innerHTML += "Введите корректную фамилию<br>";
-    } else {surname_result = true;}
+    } else {
+        surname_result = true;
+    }
     if (year.value == '' || year_len !== 4) {
         document.getElementById("message").innerHTML += "Введите корректный год рождения<br>";
-    } else {year_result = true;}
+    } else {
+        year_result = true;
+    }
     if (email.value == '' || !email.value.match(mailformat)) {
         document.getElementById("message").innerHTML += "Введите корректную электронную почту<br>";
-    } else {email_result = true;}
+    } else {
+        email_result = true;
+    }
     if (password.value == '' || password_len >= 20 || password_len < 5) {
         document.getElementById("message").innerHTML += "Пароль должен содержать от 5 до 20 символов<br>";
-    } else {password_result = true;}
+    } else {
+        password_result = true;
+    }
     if (password2.value == '') {
         document.getElementById("message").innerHTML += "Введите пароль повторно<br>";
-    } else {password2_result = true;}
+    } else {
+        password2_result = true;
+    }
     if (password.value !== password2.value) {
         document.getElementById("message").innerHTML += "Пароли не совпадают<br>";
-    }  else {passwords_result = true;}
-    
+    } else {
+        passwords_result = true;
+    }
+
     if (name_result == true && surname_result == true && year_result == true && email_result == true && password_result == true && password2_result == true && passwords_result == true) {
         document.getElementById("message").innerHTML += 'Добро пожаловать, ' + name.value + '!';
+        return true;
     }
 }
