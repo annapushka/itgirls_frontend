a05b0ebccd226672e36635b2c0fab7b32c4171fc
Anna Pushkareva <smirnova_spb@mail.ru>
2023-02-20 21:37:32 +0300

    3 week theory

--- TypeScript_01/app.ts
+++ TypeScript_01/app.ts
@@ -69,6 +69,115 @@
     // 5.	Создайте переменную собственного типа, а потом инициализируйте её значение.
 
     type stumber = string | number;
-    let test: stumber = 12
+    let test: stumber = 12;
+
+    //Неделя.3
+
+    (function (): void {
+        console.log('Anonymous function')
+    });
+
+    let show = function (): void {
+        console.log('Anonymous function');
+    };
+
+    setTimeout(function (): void {
+        console.log('Anonymous function')
+    }, 1000);
+
+
+    function getName(firstName: string, lastName ? : string): string {
+        return `${firstName} ${lastName}`;
+    }
+
+    function getSum(a: number, b: number, c ? : number): number {
+        if (c) {
+            return a + b + c;
+        }
+        return a + b;
+    }
+
+    function sayGoodNight(goodNight: string, name ? : string): string {
+        if (name) {
+            return `${goodNight} ${name}`;
+        }
+        return goodNight;
+    }
+
+    function getNameSmb(firstName: string, lastName: string = "007"): string {
+        return `${firstName} ${lastName}`;
+    }
+
+    function getSumSmth(a: number, b: number, c: number = 10): number {
+        return a + b + c;
+    }
+
+    function sayGoodNightSmb(goodNight: string, name: string = "colleague"): string {
+        return `${goodNight} ${name}`;
+    }
+
+    function getDate(): number {
+        return Date.now()
+    }
+
+    function printNames(names: Array < string > ): string {
+        return names.toString();
+    }
+
+    function printAnimals(intro: string, ...animals: string[]): string {
+        return `${intro} ${animals.join(", ")}.`;
+    }
+
+    function sum(...theArgs: number[]): number {
+        let total = 0;
+        for (const arg of theArgs) {
+            total += arg;
+        }
+        return total;
+    }
+
+    function multiply(multiplier: number, ...theArgs: number[]) {
+        return theArgs.map((element) => multiplier * element);
+    }
+
+// 1. Если переменная a  равна нулю или равна двум, то прибавьте к ней 7, иначе поделите её на 10.
+// Выведите новое значение переменной на экран. Проверьте работу скрипта при a, равном 5, 0, -3, 2.
+// Выполните задание с помощью логических операторов.
+
+
+// 2. Если переменная a равна нулю, то выведите в консоль 'Верно', иначе выведите 'Неверно'.
+// Проверьте работу скрипта при трёх разных значениях a. Выполните задание с помощью логических операторов и с помощью if.
+
+
+// 3. Переменная lang может принимать 2 значения: 'ru' 'en'. Если она имеет значение 'ru', то в переменную arr 
+// запишем массив дней недели на русском языке, а если имеет значение 'en', то на английском. Решите задачу через if и 
+// через switch-case.
+
+
+// 4. С помощью логических ветвлений рассчитайте, какую сумму работник должен перечислить на налог и какую сумму он
+//  получит на руки, если налоги начисляются таким образом:
+// ```jsx
+// Зарплата до 6000 включительно — 10%;
+// Зарплата до 10 000 включительно — 15%;
+// Зарплата до 15 000 включительно — 17.5%;
+// Зарплата свыше 15 000 — 20%.
+// ```
+
+
+// 5. Выведите столбец чисел от 1 до 50 с помощью цикла.
+
+
+// 6. Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for выведите все числа в столбец вместе с текстом ‘Результат: ’.
+
+
+// 7. У нас есть объект
+// ```jsx
+// let obj = {
+// 	'кот': 'мяу',
+// 	'собака': 'гав',
+// 	'корова': 'муу',
+// };
+// ```
+// С помощью цикла for-in выведите на экран строки такого формата: 'кот говорит мяу'.
 
 })();
