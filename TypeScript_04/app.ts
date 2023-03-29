a0f08e34e3e8315ce2a8654abe167e7ef79a36e8
Anna Pushkareva <smirnova_spb@mail.ru>
2023-03-29 21:40:49 +0300

    first commit

--- TypeScript_04/app.ts
+++ TypeScript_04/app.ts
@@ -2,5 +2,10 @@ import { Button } from './classes/Button'
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplay = document.querySelector('#result')
+    let width = 15
+    let currentShooterIndex = 202
+    let currentInvaderIndex = 0
     const button = new Button('tstBtn');
 });
