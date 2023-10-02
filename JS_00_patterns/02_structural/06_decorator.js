2e324de6a369b9de3023d8a74707b94e6f4c7b41
Anna Pushkareva <smirnova_spb@mail.ru>
2023-10-02 13:37:07 +0300

    JS: add decorator pattern

---
+++ JS_00_patterns/02_structural/06_decorator.js
@@ -0,0 +1,30 @@
+class Server {
+    constructor(ip, port) {
+        this.ip = ip
+        this.port = port
+    }
+
+    get url() {
+        return `https://${this.ip}:${this.port}`
+    }
+}
+
+function aws(server) {
+    server.isAWS = true
+    server.awsInfo = function() {
+        return server.url
+    }
+    return server
+}
+
+function azure(server) {
+    server.isAzure = true
+    server.port += 500
+    return server
+}
+
+const s1 = aws(new Server('00000000000', 8080))
+console.log(s1.awsInfo())
+
+const s2 = azure(new Server('00000000000', 1000))
+console.log(s2.url) // https://00000000000:1500
