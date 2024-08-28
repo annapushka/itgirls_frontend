9149ce8bca6210362d992761b579bdcfc83ff1ed
anna <anna.pushkareva@smddev.com>
2024-08-28 11:29:22 +0300

    getSumFromString node js example

--- JS_19_CodeWars/5_directions_reduction.js
+++ JS_19_CodeWars/5_directions_reduction.js
@@ -19,3 +19,19 @@ function dirReduc(directions) {
 
     return stack;
 }
+
+
+function getSumFromString(string) {
+    const numbers = string.split('').map(Number);
+    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+    return sum;
+}
+
+const fs = require('fs')
+let fileContent = fs.readFileSync("input.txt", "utf8");
+
+const [a, b] = fileContent.toString().split(' ')
+
+const result = Number(a) + Number(b)
+
+fs.writeFileSync("output.txt", result.toString())
